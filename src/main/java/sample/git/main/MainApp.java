1749078e9a96c51f3174211889fee67c5059573b
madahada <Madahada@web.de>
2017-02-05 15:44:13 +0100

    Initial commit

---
+++ src/main/java/sample/git/main/MainApp.java
@@ -0,0 +1,10 @@
+package sample.git.main;
+
+/**
+ * Created by sde52a6 on 05.02.2017.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("ein test");
+    }
+}
